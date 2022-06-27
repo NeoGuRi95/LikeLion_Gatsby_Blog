c02a25a5cdd72dc0178baafd6b4538cdb70687b5
NeoGuRi95 <jea5158@gmail.com>
2022-06-27 17:16:29 +0900

    테스트 페이지 추가

---
+++ src/pages/test-space.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Test Space Page</title>
+      <h1>Test Space Page</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
