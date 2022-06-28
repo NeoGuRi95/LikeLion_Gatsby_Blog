979edea8278cd77cc45a47fc446d13a09582d7ab
NeoGuRi95 <jea5158@gmail.com>
2022-06-28 14:35:09 +0900

    static image 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
+// gatsby-config.js
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
