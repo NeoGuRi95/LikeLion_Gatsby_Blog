9585b6467b0a4bf00c3c1ef02ef72c497e518432
NeoGuRi95 <jea5158@gmail.com>
2022-06-28 13:22:06 +0900

    Head 파일 삭제, test/test-space 파일 삭제

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,8 +8,6 @@ function Layout({ children }) {
         <div style={{ display: "flex", gap: 10 }}>
           <Link to="/">홈</Link>
           <Link to="/about">어바웃</Link>
-          <Link to="/test">테스트</Link>
-          <Link to="/test-space">테스트-스페이스!!</Link>
         </div>
 
         {children}
